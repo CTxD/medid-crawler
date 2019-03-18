cd577c393fc6371fad82c3e714887b9725543ab3
Dennis Juhl Østergaard <dala15@student.aau.dk>
2019-03-18 15:45:09 +0100

    Minimalistic generic crawler implemented.

--- source/crawler/crawler.py
+++ source/crawler/crawler.py
@@ -1,7 +1,11 @@
 import requests
+import bs4
 
-from ..config import CONFIG
+# from ..config import CONFIG
 
 
-def crawl(url: str) -> None: # BS4 
+def crawl(url: str) -> bs4.BeautifulSoup:
     response = requests.get(url)
+    source = bs4.BeautifulSoup(response.text.encode('utf-8'), 'html.parser')
+
+    return source
