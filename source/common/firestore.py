c3d403b3a51dc66b78e8f9334dac336345bd5b24
Dennis Juhl Østergaard <dala15@student.aau.dk>
2019-04-04 14:59:08 +0200

    Added firebase_admin requirement to common requirements. Implemented firestore as class to avoid global certificate loading. Adjusted tests for firestore to reflect this change + added mocking of certificate file to testing aswell. 100% test coverage.

--- source/common/firestore.py
+++ source/common/firestore.py
@@ -8,43 +8,43 @@ from source.config import CONFIG
 
 
 logger = logging.getLogger(__name__)
-credentials = credentials.Certificate(
-    os.path.join(os.getcwd(), CONFIG["CERT"])
-)
-medId = initialize_app(credentials)
-db = firestore.client()
-
-def _convert_obj_to_dict(obj):
-    class_dict = {}
-    for attr in dir(obj):
-        if attr.startswith('__'):
-            continue
-
-        attrvalue = getattr(obj, attr)
-
-        if callable(attrvalue):
-            continue
-
-        if isinstance(attrvalue, list):
-            newattrvalue = []
-            for oldvalue in attrvalue:
-                newattrvalue.append(_convert_obj_to_dict(oldvalue))
-
-            attrvalue = newattrvalue
-
-        class_dict[attr] = attrvalue
-
-    return class_dict
-
-def add_or_update(collection_id: str, pill_object: pill.PillData):
-        temp = _convert_obj_to_dict(pill_object)
-        db.collection(collection_id).document(pill_object.pillname).set(temp)
 
 
-def get(collection_id: str, document_id: str):
-    return db.collection(collection_id).document(document_id).get()
+class FBManager():
+    def __init__(self):
+        certificate = credentials.Certificate(
+            os.path.join(os.getcwd(), CONFIG["CERT"])
+        )
+        self.db = firestore.client(initialize_app(certificate))
 
+    def _convert_obj_to_dict(self, obj):
+        class_dict = {}
+        for attr in dir(obj):
+            if attr.startswith('__'):
+                continue
 
-def get_all(collection_id: str):
-    return db.collection(collection_id).get()
+            attrvalue = getattr(obj, attr)
 
+            if callable(attrvalue):
+                continue
+
+            if isinstance(attrvalue, list):
+                newattrvalue = []
+                for oldvalue in attrvalue:
+                    newattrvalue.append(self._convert_obj_to_dict(oldvalue))
+
+                attrvalue = newattrvalue
+
+            class_dict[attr] = attrvalue
+
+        return class_dict
+
+    def add_or_update(self, collection_id: str, pill_object: pill.PillData):
+        temp = self._convert_obj_to_dict(pill_object)
+        self.db.collection(collection_id).document(pill_object.pillname).set(temp)
+
+    def get(self, collection_id: str, document_id: str):
+        return self.db.collection(collection_id).document(document_id).get()
+
+    def get_all(self, collection_id: str):
+        return self.db.collection(collection_id).get()
