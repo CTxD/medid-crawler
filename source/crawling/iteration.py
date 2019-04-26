b9e63b1109da1ccb2647e116addae5c25e11398e
Dennis Juhl Østergaard <dala15@student.aau.dk>
2019-04-26 09:34:36 +0200

    Fixed bug that caused the crawling to crash upon 2nd consequtive crawl iteration.

--- source/crawling/iteration.py
+++ source/crawling/iteration.py
@@ -8,13 +8,21 @@ from . import druginfopageextracter
 
 
 logger = logging.getLogger(__name__)
+_fbm = None 
+
+
+def getFBM(): # noqa
+    global _fbm
+    if _fbm is None:
+        _fbm = fs.FBManager()
+    return _fbm
 
 
 # REMOVE PRAGMA: NO COVER WHEN FUNCTION RECEIVES ACTUAL IMPLEMENTATION!
 def start(): # pragma: no cover
     # 1. Get the list of drugs
     druglistdict = druglistextractor.getdruglinklist()
-    pilldatabase = fs.FBManager()
+    pilldatabase = getFBM()
     # 2. Give the list of drugs to the druginfoextractor and
     # 3. Feed the database with drugs from (above)
     logger.info('Getting drug info')
--- tests/crawling/test_iteration.py
+++ tests/crawling/test_iteration.py
@@ -1,4 +1,26 @@
 from source.crawling import iteration
+from source.common import firestore
+
+def test_getFBM_uninitialized(firebasemanager, monkeypatch): # noqa
+    # Hard-set the _fbm to None
+    iteration._fbm = None # noqa
+
+    def getfirebasemanagerfixture():
+        return firebasemanager
+    
+    # Monkeypatch firestore.FBManager() to return the firebasemanager obj
+    original = firestore.FBManager
+    firestore.FBManager = getfirebasemanagerfixture
+    fbm = iteration.getFBM()
+    assert isinstance(fbm, original)
+
+    firestore.FBManager = original
+    
+
+def test_getFBM_initialized(firebasemanager): # noqa
+    iteration._fbm = firebasemanager # noqa
+
+    assert iteration.getFBM() == firebasemanager # noqa
 
 
-### MAKE TESTS FOR ITERATION FILE WHEN IT BEGINS TO ACTUALLY WRAP FUNCTIONALITY!
+# ## MAKE TESTS FOR ITERATION FILE WHEN IT BEGINS TO ACTUALLY WRAP FUNCTIONALITY!
